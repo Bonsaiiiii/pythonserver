58cd5badc7e1f820a3c20b59803b70cc4eff90c0
Caio Daniel Hames de Souza <138807198+Bonsaiiiii@users.noreply.github.com>
2025-06-23 15:57:28 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -150,6 +150,6 @@ def delete_file():
         return jsonify({"error": f"Error deleting file: {str(e)}"}), 500
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
 
